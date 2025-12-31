e2100506d1c32aeff1595430eba099df20f69c26
skyz <notskyzzo@gmail.com>
2025-12-31 12:20:11 +0100

    Auth, design for main page.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <a href="/register">Register</a>
+    <a href="/login">Login</a>
+</body>
+</html>
